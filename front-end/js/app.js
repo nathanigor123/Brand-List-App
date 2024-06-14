8bed8bbc6c3e73874b0d064acbd99492331e51ea
nathanigor123 <igornathan777@gmail.com>
2024-06-14 23:56:22 +0100

    dockerizing front-end-html

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     fetchTopList();
   });
   
-  const API_BASE_URL = "http://127.0.0.1:8000/api";
+  const API_BASE_URL = "http://localhost:8800/api";
   
   function fetchTopList() {
     fetch(`${API_BASE_URL}/brands/toplist`)
